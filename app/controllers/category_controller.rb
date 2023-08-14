c3d5e31a0762abdc55aa0c74090632113f511ccb
anbehindY <yanminthwin@gmail.com>
2023-08-15 02:46:56 +0630

    Create categories page

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,4 +1,6 @@
 class CategoryController < ApplicationController
   def index
+    return unless user_signed_in?
+    @categories = current_user.categories
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many  :bills, foreign_key: :author_id, dependent: :destroy
-  has_many  :categories, foreign_key: :author_id, dependent: :destroy
+  has_many  :categories, foreign_key: :user_id, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
