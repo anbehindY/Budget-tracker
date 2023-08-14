e56dc0ada09ef652e51e9c4ce6086c3822411967
anbehindY <yanminthwin@gmail.com>
2023-08-14 11:02:05 +0630

    Add validations and associations to models

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,7 @@
 class Bill < ApplicationRecord
   belongs_to :author
+  has_and_belongs_to_many :categories
+  
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many  :bills, foreign_key: :author_id, dependent: :destroy
+  has_many  :categories, foreign_key: :author_id, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
