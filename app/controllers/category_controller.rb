b19f552b230fe44f549050def625644ad24e7067
anbehindY <yanminthwin@gmail.com>
2023-08-15 07:05:35 +0630

    Implement add category function

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -3,4 +3,23 @@ class CategoryController < ApplicationController
     return unless user_signed_in?
     @categories = current_user.categories
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.create(category_params)
+    if @category.save
+      redirect_to category_index_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
